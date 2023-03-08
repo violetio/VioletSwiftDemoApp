ad8fc607a210e90ebdf29df356d8542147c6c3da
alandmorford <alan@violet.io>
2023-03-08 15:12:31 -0800

    Extract HelloView

---
+++ Checkout/Checkout/Views/HelloView.swift
@@ -0,0 +1,23 @@
+//
+//  HelloView.swift
+//  Checkout
+//
+//  Created by Alan Morford on 3/8/23.
+//
+
+import SwiftUI
+
+struct HelloView: View {
+    var body: some View {
+        Image(systemName: "globe")
+            .imageScale(.large)
+            .foregroundColor(.accentColor)
+        Text("Hello, world!")
+    }
+}
+
+struct HelloView_Previews: PreviewProvider {
+    static var previews: some View {
+        HelloView()
+    }
+}

ef78cd7774b6483a9a6e031734df8636410edbbe
alandmorford <alan@violet.io>
2023-03-08 12:50:12 -0800

    Create Project and Workspace

---
+++ Checkout/Checkout/CheckoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckoutApp.swift
+//  Checkout
+//
+//  Created by Alan Morford on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CheckoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

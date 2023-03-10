19b83930462e82e56d492e33514211a4a51b44ae
alandmorford <alan@violet.io>
2023-03-10 12:27:02 -0800

    A login button to test the loginPost call

--- Checkout/Checkout/ContentView.swift
+++ Checkout/Checkout/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HelloView()
+            LoginButtonView()
             SelectedOfferView()
                 .padding()
         }

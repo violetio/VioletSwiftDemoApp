06a56c32db43614ea85853144d905e95f4988647
alandmorford <alan@violet.io>
2023-03-27 13:32:42 -0700

    Split out ExpectationRunner

--- Checkout/CheckoutTests/APIModelsTests.swift
+++ Checkout/CheckoutTests/APIModelsTests.swift
@@ -11,32 +11,7 @@ import SwiftUI
 import VioletPublicClientAPI
 import XCTest
 
-class ExpectationRunner {
-    let expectation = XCTestExpectation(description: "CallCompleted True")
-    let baseAPICall: BaseAPICall
-    var streamHandle: AnyCancellable?
-    var expectations: [XCTestExpectation] {
-        return [self.expectation]
-    }
 
-    init(_ baseAPICall: BaseAPICall) {
-        self.baseAPICall = baseAPICall
-    }
-
-    private func publisher() -> Publishers.Drop<Published<Bool>.Publisher> {
-        return self.baseAPICall.$callCompleted.dropFirst()
-    }
-
-    typealias PublisherOutput = Publishers.Drop<Published<Bool>.Publisher>.Output
-
-    func sink(outputValue: @escaping ((PublisherOutput) -> Void)) {
-        self.streamHandle = self.publisher()
-            .sink(receiveValue: {
-                outputValue($0)
-                self.expectation.fulfill()
-            })
-    }
-}
 
 final class APIModelsTests: APIXCTestCase {
     var refreshToken: String? = nil

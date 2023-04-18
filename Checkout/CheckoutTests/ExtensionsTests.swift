74158dc4e6230f2b1e9aab54c058e5ac63b04923
alandmorford <alan@violet.io>
2023-04-18 13:31:32 -0700

    Extend Offer to get the first Album Media SourceURL

---
+++ Checkout/CheckoutTests/ExtensionsTests.swift
@@ -0,0 +1,27 @@
+//
+//  ExtensionsTests.swift
+//  CheckoutTests
+//
+//  Created by Alan Morford on 4/18/23.
+//
+
+import XCTest
+@testable import Checkout
+import VioletPublicClientAPI
+
+final class ExtensionsTests: XCTestCase {
+
+
+    func test_firstAlbumMediaImageURL() throws {
+        guard let offer = MockOffers.load_Offer_12555() else {
+            XCTFail("Missing offer to test")
+            return
+        }
+        
+        XCTAssertNotNil(offer.firstAlbum())
+        XCTAssertNotNil(offer.firstAlbumMedia())
+        XCTAssertNotNil(offer.firstAlbumMediaImageSourceUrl())
+        XCTAssertNotNil(offer.firstAlbumMediaImageURL())
+    }
+
+}

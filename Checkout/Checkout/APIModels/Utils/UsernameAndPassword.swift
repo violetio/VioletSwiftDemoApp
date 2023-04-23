7e2e9ece22581962913d29467416aeeb798e7ac4
alandmorford <alan@violet.io>
2023-04-23 08:59:26 -0700

    DemoUsernameAndPassword

---
+++ Checkout/Checkout/APIModels/Utils/UsernameAndPassword.swift
@@ -0,0 +1,12 @@
+//
+//  UsernameAndPassword.swift
+//  Checkout
+//
+//  Created by Alan Morford on 4/23/23.
+//
+
+protocol UsernameAndPassword {
+    var username: String { get }
+    var password: String { get }
+}
+ 

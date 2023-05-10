70ab38dfe3da6def2207a7634f06aed3d3596f18
alandmorford <alan@violet.io>
2023-05-10 07:49:03 -0700

    Split out Sender type for running actions and updating state

--- Checkout/Checkout/Models/RootState/AppStore.swift
+++ Checkout/Checkout/Models/RootState/AppStore.swift
@@ -43,56 +43,12 @@ class AppStore {
     }
 
     func send(_ action: AppAction) {
-        switch action {
-        case .changeActiveAppID(let newAppIDAndSecret):
-            Logger.info("Store: Setting channelLoginViewState.appIDandSecret")
-            channelLoginViewState.channelHeaders = nil
-            channelLoginViewState.appIDandSecret = newAppIDAndSecret
-        case .logout:
-            channelLoginViewState.channelHeaders = nil
-        case .refreshAuthTokenRequest(let channelHeaders):
-            Logger.info("Store: Send Refresh Auth Token Request")
-            let newAPICall = APICall(apiCall: AuthTokenGet(channelHeaders: channelHeaders))
-            pendingAPICalls.enqueue(newAPICall)
-            newAPICall.send { dataResponse, _ in
-                if let refreshResponse = dataResponse,
-                   let newAuthToken = refreshResponse.token
-                {
-                    Logger.info("Got refreshResponse")
-                    self.channelLoginViewState.channelHeaders = channelHeaders.replaceToken(token: newAuthToken)
-                }
-            }
-
-        case .loginRequest(let loginInputs):
-            Logger.info("Store: Send Login Request: \(loginInputs.username)")
-            let newAPICall = APICall(apiCall: LoginPostRequest(loginInputs: loginInputs))
-            pendingAPICalls.enqueue(newAPICall)
-            newAPICall.send { dataResponse, _ in
-                if let loginResponse = dataResponse {
-                    Logger.info("Got LoginResponse")
-                    self.channelLoginViewState.channelHeaders = ChannelHeaders.from(loginResponse: loginResponse, appIdAndSecret: loginInputs)
-                }
-            }
-
-        case .offersPageRequest(let channelHeaders, let merchantId):
-            Logger.info("Store: Offers Page Request:")
-            let newAPICall = APICall(apiCall: GetPageOffersByMerchantIDRequest(channelHeaders: channelHeaders, merchantId: merchantId))
-            pendingAPICalls.enqueue(newAPICall)
-            newAPICall.send { dataResponse, _ in
-                if let pageOffer = dataResponse {
-                    Logger.info("Got PageOffer")
-                    self.shoppingViewState.mergePageOffer(pageOffer)
-                }
-            }
-        case .createCartRequest:
-            Logger.info("Store: Create Cart Request:")
-        case .updateCartCustomerRequest:
-            Logger.info("Store: Create Cart Request:")
-        }
+        sender.send(action)
     }
 
     let state: AppState
-    let pendingAPICalls = PendingAPICalls()
+    let sender: AppSender
+    
     static let mockAppStore = AppStore(shoppingViewState: ShoppingViewState(offerPath: [],
                                                                             loadedOfferItems: PreviewMocks.MockOfferItemsArray(),
                                                                             offerItemPath: []))
@@ -113,9 +69,11 @@ class AppStore {
          demoChannelViewState: DemoChannelViewState,
          shoppingViewState: ShoppingViewState)
     {
-        self.state = AppState(channelLoginViewState: channelLoginViewState,
-                              demoChannelViewState: demoChannelViewState,
-                              shoppingViewState: shoppingViewState)
+        let newState = AppState(channelLoginViewState: channelLoginViewState,
+                                demoChannelViewState: demoChannelViewState,
+                                shoppingViewState: shoppingViewState)
+        self.state = newState
+        self.sender = AppSender(state: newState)
     }
 
     convenience init(shoppingViewState: ShoppingViewState? = nil) {

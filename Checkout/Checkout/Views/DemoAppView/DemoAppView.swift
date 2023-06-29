17c7c52d77ecd69939df9be11909876b6689e73d
alandmorford <alan@violet.io>
2023-06-29 13:53:05 -0700

    Refine NavBarCartButton

--- Checkout/Checkout/Views/DemoAppView/DemoAppView.swift
+++ Checkout/Checkout/Views/DemoAppView/DemoAppView.swift
@@ -14,18 +14,44 @@ struct DemoAppView: View {
         }
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
-            ToolbarItem(placement: .principal) {
+            ToolbarItem(placement: .navigationBarLeading) {
                 HStack {
                     Text("Demo App")
                         .font(.system(size: 25, weight: .bold))
                       .foregroundColor(.black)
                     Spacer()
-                    Image(systemName: "cart")
+
                 }.padding(.horizontal)
+                
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                NavBarCartButton(action: {
+                    // Add your button action here
+                    print("Custom button tapped!")
+                }, buttonText: "0")
             }
         }
     }
 }
+
+struct NavBarCartButton: View {
+    var action: () -> Void
+    var buttonText: String
+    
+    var body: some View {
+        Button(action: action) {
+            HStack(spacing: 10) {
+                Image(systemName: "cart.fill")
+                Text(buttonText)
+                    .font(.system(size: 17, weight: .semibold))
+            }.padding(.horizontal, 20)
+                .padding(.vertical, 13)
+                .cornerRadius(10)
+                //.border(.black)
+        }
+    }
+}
+
 
 struct DemoAppView_Previews: PreviewProvider {
     static var previews: some View {

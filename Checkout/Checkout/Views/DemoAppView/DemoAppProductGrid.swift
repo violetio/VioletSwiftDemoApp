b57b75b4927d7a62762c7fc2a4bbf0dce1f1b402
alandmorford <alan@violet.io>
2023-07-31 13:50:46 -0700

    Use special icon in Label for Merchant Name

--- Checkout/Checkout/Views/DemoAppView/DemoAppProductGrid.swift
+++ Checkout/Checkout/Views/DemoAppView/DemoAppProductGrid.swift
@@ -20,12 +20,12 @@ struct DemoAppProductGrid: View {
     
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: layout, spacing: 15) {
+            LazyVGrid(columns: layout, spacing: 20) {
                 ForEach(loadedOffers, id: \.offer_id) { offerItem in
                     DemoAppOfferCard(store: $store,
                                      offerItem: .constant(offerItem))
                 }
-            }.offset(CGSize(width: 0, height: 15)) //This pushes scroll content top y down 15 pts
+            }.offset(CGSize(width: 0, height: 20)) //This pushes scroll content top y down 15 pts
                 .frame(minWidth: 390) //This matches the scrollview width to parent view width (at least on iPhone 14
                 .background(Color(red: 0.95, green: 0.95, blue: 0.97))
                 

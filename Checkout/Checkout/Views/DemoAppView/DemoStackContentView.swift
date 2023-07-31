77fda09d1aec308d5fb3afd91ed5e2c8f1ff1cbd
alandmorford <alan@violet.io>
2023-07-31 10:54:58 -0700

    Add DemoStackContentView as main ContentView

---
+++ Checkout/Checkout/Views/DemoAppView/DemoStackContentView.swift
@@ -0,0 +1,25 @@
+//
+//  DemoStackContentView.swift
+//  Checkout
+//
+//  Created on 7/31/23
+//
+
+import SwiftUI
+
+struct DemoStackContentView: View {
+    @Binding var store: AppStore
+    
+    var body: some View {
+        NavigationStack
+        {
+            DemoAppView(store: $store)
+        }
+    }
+}
+
+struct DemoStackContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        DemoStackContentView(store: AppStore.mockAppStoreBinding)
+    }
+}

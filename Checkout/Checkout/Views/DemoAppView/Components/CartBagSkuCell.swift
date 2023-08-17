26bea67a84b5545a10191d0c28fac9f3ec631955
alandmorford <alan@violet.io>
2023-08-17 13:47:43 -0700

    Start structure cell

--- Checkout/Checkout/Views/DemoAppView/Components/CartBagSkuCell.swift
+++ Checkout/Checkout/Views/DemoAppView/Components/CartBagSkuCell.swift
@@ -10,14 +10,23 @@ import SwiftUI
 struct CartBagSkuCell: View {
     @ObservedObject var orderSkuViewState: OrderSkuViewState
     var body: some View {
-        Text("OrderSkuId: \(orderSkuViewState.orderSkuID)")
+        HStack {
+            
+            VStack {
+                Text("OrderSkuId: \(orderSkuViewState.orderSkuID)")
+            }
+        }
+        
     }
 }
 
 struct CarBagOrderSkuListCell_Previews: PreviewProvider {
     static let mockOrder = MockOffers.load_OrderID_71169()!
+    static let mockBag = mockOrder.bags!.first!
+    static let mockSku1 = mockBag.skus![0]
+    static let mockSku2 = mockBag.skus![0]
     
     static var previews: some View {
-        CartBagSkuCell(orderSkuViewState: OrderSkuViewState(orderSku: mockOrder.bags![0].skus![0]))
+        CartBagSkuCell(orderSkuViewState: OrderSkuViewState(orderSku: mockSku1))
     }
 }

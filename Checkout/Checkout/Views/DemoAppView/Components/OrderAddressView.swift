1cba21e656cd8b97d83d743913aa0f7990a4950f
alandmorford <alan@violet.io>
2023-08-24 14:28:39 -0700

    Country Picker

--- Checkout/Checkout/Views/DemoAppView/Components/OrderAddressView.swift
+++ Checkout/Checkout/Views/DemoAppView/Components/OrderAddressView.swift
@@ -17,7 +17,24 @@ struct OrderAddressView: View {
             FormTextField(orderAddressViewState.addressLine1Prompt, text: $orderAddressViewState.address1)
             FormTextField(orderAddressViewState.addressLine2Prompt, text: $orderAddressViewState.address2)
             FormTextField(orderAddressViewState.cityPrompt, text: $orderAddressViewState.city)
-            FormTextField(orderAddressViewState.countryPrompt, text: $orderAddressViewState.country)
+//            FormTextField(orderAddressViewState.countryPrompt, text: $orderAddressViewState.country)
+            
+            Menu {
+                Picker("Country", selection: $orderAddressViewState.country) {
+                    ForEach(CountryCodesArray.loadJson(), id: \.self) { pickValue in
+                        Text(pickValue).font(.system(size: 12, weight: .semibold))//.tag(pickValue)
+                    }
+                }
+            } label: {
+                LabeledContent("Country", value: orderAddressViewState.country)
+                    .font(Font.custom("SF Pro Text", size: 12))
+                    .padding(.horizontal)
+                    .frame(width: 340, height: 44)
+                    .foregroundColor(.black)
+            }
+            .background(Color.white)
+            .cornerRadius(12)
+            .padding(.vertical, 5)
             FormTextField(orderAddressViewState.postalCodePrompt, text: $orderAddressViewState.postalCode)
             FormTextField(orderAddressViewState.statePrompt, text: $orderAddressViewState.state)
         }
@@ -26,6 +43,10 @@ struct OrderAddressView: View {
 
 struct OrderAddressView_Previews: PreviewProvider {
     static var previews: some View {
-        OrderAddressView(orderAddressViewState: OrderAddressViewState())
+        ScrollView {
+            OrderAddressView(orderAddressViewState: OrderAddressViewState())
+        }.frame(width: 390)
+            .navigationTitle("Guest Checkout").withScrollViewBackgroundColor()
     }
 }
+
